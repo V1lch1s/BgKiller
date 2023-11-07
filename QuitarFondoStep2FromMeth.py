15f05000b9027f9b8bcc8b7795bc9b5c4e56abb7
V1lch1s <143208742+V1lch1s@users.noreply.github.com>
2023-11-07 14:25:41 -0600

    QuitarFondoStep2FromMeth.py

--- QuitarFondoStep2FromMeth.py
+++ QuitarFondoStep2FromMeth.py
@@ -6,7 +6,7 @@ import cv2
 print("----Eliminador de Fondo (Transparentador de pngs)----")
 print("\nRequisitos de entrada:\n")
 print("1-La imagen de entrada debe ser PNG\n")
-print("2-El fondo debe ser RGB(34, 177, 76)\n\n-Recomendación: Editar imagen en Microsoft Paint ya que el color requerido\n\t\tes el verde oscuro que aparece por defecto en la aplicación.\n\n")
+print("2-El fondo debe ser RGB(0, 255, 0)\n\n")
 carpeta = str(input('Carpeta (Nombre): '))
 imgName = str(input("Imagen que desea alterar (Imagen.png): "))
 img = Image.open(carpeta+'/'+imgName)
@@ -16,7 +16,7 @@ img = img.convert('RGBA')
 pixdata = img.load()
 for y in range(img.size[1]):
     for x in range(img.size[0]):
-        if pixdata[x,y][0] in range(15, 137) and pixdata[x,y][1] in range(124, 200) and pixdata[x,y][2] in range(40, 131):
+        if pixdata[x,y][0] == 0 and pixdata[x,y][1] == 255 and pixdata[x,y][2] == 0:
            pixdata[x, y] = (255, 255, 255,0)
 
 img.save("{}/{}".format(carpeta,imgName)) #Image.save(fp, format=None, **params)
